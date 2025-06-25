aec4efc52e5e9c31e1a9228a232a8cc0c8eb1099
Muhammed najin N <146701336+MuhammedNajin@users.noreply.github.com>
2025-06-25 15:31:31 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.js
@@ -0,0 +1,10 @@
+// Last updated: 6/25/2025, 3:31:30 PM
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var sum = function(num1, num2) {
+    
+    return num1 + num2;
+};
