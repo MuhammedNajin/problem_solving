c0d0c473207a8b4e6f972f25453171f91eaa3748
Muhammed najin N <146701336+MuhammedNajin@users.noreply.github.com>
2025-06-25 15:33:17 +0530

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.js
@@ -0,0 +1,27 @@
+// Last updated: 6/25/2025, 3:33:17 PM
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function(score) {
+    const lenScore = score.length;
+    const answer = new Array(lenScore);
+    const scoreWithIndex = score.map((score, index) => [score, index]);
+
+    // Sort the score in descending order
+    scoreWithIndex.sort((a, b) => b[0] - a[0]);
+
+    const rank = ["Gold Medal", "Silver Medal", "Bronze Medal"];
+
+    // Assign ranks based on sorted score
+    for (let i = 0; i < lenScore; i++) {
+        const index = scoreWithIndex[i][1];
+        if (i < 3) {
+            answer[index] = rank[i];
+        } else {
+            answer[index] = String(i + 1);
+        }
+    }
+
+    return answer;
+};
