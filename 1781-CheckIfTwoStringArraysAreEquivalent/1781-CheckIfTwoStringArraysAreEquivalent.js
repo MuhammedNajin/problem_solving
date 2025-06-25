245f085bc829a5c225843773a5345e8bafce76c8
Muhammed najin N <146701336+MuhammedNajin@users.noreply.github.com>
2025-06-25 15:32:28 +0530

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.js
@@ -0,0 +1,9 @@
+// Last updated: 6/25/2025, 3:32:27 PM
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+   return word1.join('') === word2.join('')
+};
