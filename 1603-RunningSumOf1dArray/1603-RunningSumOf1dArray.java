7ef4ace24149fbd267e4c7b48cd73e2adb833edd
Muhammed najin N <146701336+MuhammedNajin@users.noreply.github.com>
2025-06-25 15:32:37 +0530

    Create 1603-RunningSumOf1dArray

---
+++ 1603-RunningSumOf1dArray/1603-RunningSumOf1dArray.java
@@ -0,0 +1,13 @@
+// Last updated: 6/25/2025, 3:32:36 PM
+class Solution {
+     int i;
+    public int[] runningSum(int[] nums) {
+
+        for(i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
+        }
+        return nums;
+    }
+}
+
+   
