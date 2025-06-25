9798f8897b7ad55402f52c9258792ede8e8d7f7e
Muhammed najin N <146701336+MuhammedNajin@users.noreply.github.com>
2025-06-25 15:32:31 +0530

    Create 1651-ShuffleString

---
+++ 1651-ShuffleString/1651-ShuffleString.js
@@ -0,0 +1,20 @@
+// Last updated: 6/25/2025, 3:32:31 PM
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+
+    let arr = new Array();
+
+    s = s.split('');
+
+    for(let i = 0; i < s.length; i++) {
+       
+       arr[indices[i]] = s[i];
+
+    }
+   return arr.join('');
+    
+};
